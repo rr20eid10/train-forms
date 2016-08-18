e3bb5fdc4a01daef6030d05a77ed78d2d641ec8c
rr20eid10 <rr20eid10@users.noreply.github.com>
2016-08-17 20:27:26 -0400

    initial commit
    added all files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $(".reg-price").on("mouseenter", function(){
+
+      if($(this).siblings().hasClass("dp-show")){
+        $(this).fadeOut("slow", function(){
+            $(this).parent().addClass("fd-bg-fade");
+          });
+        // console.log($(this).siblings());
+      }
+        
+
+    });
+
+});
